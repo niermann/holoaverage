c24a01907b9bc1952f0cf8f50df4b17f935724bf
Tore Niermann <tore.niermann@tu-berlin.de>
2018-08-14 09:25:05 +0200

    Moved module main code to __main__.py

---
+++ holoaverage/__main__.py
@@ -0,0 +1,24 @@
+# This file is part of holoaverage.
+# Copyright (c) 2018 Tore Niermann
+#
+# holoaverage is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with holoaverage.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function
+
+import sys
+
+from .main import main
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
