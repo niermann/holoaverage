Fixes a bug, which caused slight phase gradients if a ROI was used

holoaverage/grid.py
@@ -177,13 +177,14 @@ class Grid(object):
     # Private members
     #   _realSampling    np.ndarray[ndim,ndim]: Real space sampling in for 2D: ((YY,YX),(XY,XX)) in nm
     #   _rcprSampling    np.ndarray[ndim,ndim]: Reciprocal space sampling in 2D: ((YY,YX),(XY,XX)) in 1/nm
+    #   _realOffset      np.ndarray[ndim]: Real space offset in nm
     #   _shape           tuple: Size of simulation grid, len=ndim
     #   _complexType     np.dtype: Complex type
     #   _floatType       np.dtype: Float type
     #   _planFFT         FFT plan (destroys input)
     #   _planIFFT        IFFT plan (destroys input)
     #   _fft_nthreads    Number of threads to use in FFT plans
-    def __init__(self, shape, realSampling=None, rcprSampling=None, dtype=np.complex64):
+    def __init__(self, shape, realSampling=None, rcprSampling=None, realOffset=None, dtype=np.complex64):
         """
         Initializes grid. Either realSampling or rcprSampling must be given.
 
@@ -198,7 +199,9 @@ class Grid(object):
                 Float: Isotropic sampling in 1/nm
                 1D-array: Diagonal sampling in 1/nm
                 2D-array: Anisotropic sampling 1/nm
-            dtype
+            _realOffset:
+                2D-array: Offset 1/nm
+            d type
                 np.dtype: Data type to use
         """
         self._shape = tuple(shape)
@@ -219,6 +222,9 @@ class Grid(object):
             self._setRcprSampling(rcprSampling)
         else:
             raise ValueError("Either 'realSampling' or 'rcprSampling' must be given.")
+        self._realOffset = np.zeros(2, dtype=float)
+        if realOffset is not None:
+            self._realOffset[...] = realOffset
         self._planFFT = None
         self._planIFFT = None
         self._planRFFT = None
@@ -229,7 +235,8 @@ class Grid(object):
                 'complexType': self._complexType,
                 'floatType': self._floatType,
                 'realSampling': self._realSampling,
-                'rcprSampling': self._rcprSampling}
+                'rcprSampling': self._rcprSampling,
+                'realOffset': self._realOffset}
 
     def __setstate__(self, state):
         self._shape = state["shape"]
@@ -237,6 +244,7 @@ class Grid(object):
         self._floatType = state["floatType"]
         self._realSampling = state["realSampling"]
         self._rcprSampling = state["rcprSampling"]
+        self._realOffset = state["realOffset"]
         self._planFFT = None
         self._planIFFT = None
         self._planRFFT = None
@@ -262,7 +270,7 @@ class Grid(object):
             dim_unit = None
         dim_scale = ScaleMatrix(len(dataset.shape))
         if 'dim_scale' in dataset.attrs:
-            dim_scale.set(dataset.attrs['dim_scale']).getMatrix()
+            dim_scale.setReverse(dataset.attrs['dim_scale']).getMatrix()
         else:
             dim_scale.set(np.ones(len(dataset.shape)))
 
@@ -281,7 +289,12 @@ class Grid(object):
                     warnings.warn("Expected unit to be '1/nm' not '%s'" % dim_unit[0])
 
         if space >= 0:
-            return Grid(shape, realSampling=dim_scale, dtype=dtype)
+            if 'dim_offset' in dataset.attrs:
+                dim_offset = np.zeros(2, dtype=float)
+                dim_offset[...] = dataset.attrs["dim_offset"][::-1]
+            else:
+                dim_offset = None
+            return Grid(shape, realSampling=dim_scale, realOffset=dim_offset, dtype=dtype)
         else:
             dim_scale.set(dim_scale.getMatrix() * np.atleast_1d(shape))
             return Grid(shape, rcprSampling=dim_scale, dtype=dtype)
@@ -313,6 +326,10 @@ class Grid(object):
     def _setRcprSampling(self, value):
         self._rcprSampling.set(value)
         self._realSampling.set(np.linalg.inv(self._rcprSampling.getMatrix()))
+
+    @property
+    def realOffset(self):
+        return self._realOffset.copy()
 
     def hasDiagonalSampling(self):
         """Returns whether the sampling matrix is diagonal (sampling along grid axes)."""
@@ -483,9 +500,11 @@ class Grid(object):
             Td = np.diag(T)
             for i in range(ndim):
                 result[i] *= Td[i]
+                result[i] += self._realOffset[i]
         else:
             result = np.zeros((ndim,) + self._shape, dtype=self.floatType)
             for i in range(ndim):
                 for j in range(ndim):
-                    result[i] = result[i] + T[i, j] * idx[j]
+                    result[i] += T[i, j] * idx[j]
+                result[i] += self._realOffset[i]
         return result

holoaverage/main.py
@@ -140,9 +140,9 @@ def rescale_fourier(data, shape=None, out=None):
     elif out.shape != shape:
         raise ValueError("Output array shape must fit given shape.")
     out.fill(0)
-    out[shape[0] // 2 - half[0]:shape[0] // 2 + half[0], shape[1] // 2 - half[1]:shape[1] // 2 + half[1]] = \
-        np.fft.fftshift(np.fft.fft2(data))[data.shape[0] // 2 - half[0]:data.shape[0] // 2 + half[0],
-                                           data.shape[1] // 2 - half[1]:data.shape[1] // 2 + half[1]]
+    out[shape[0] // 2 - half[0]:shape[0] // 2 + half[0] + 1, shape[1] // 2 - half[1]:shape[1] // 2 + half[1] + 1] = \
+        np.fft.fftshift(np.fft.fft2(data))[data.shape[0] // 2 - half[0]:data.shape[0] // 2 + half[0] + 1,
+                                           data.shape[1] // 2 - half[1]:data.shape[1] // 2 + half[1] + 1]
     out[...] = np.fft.ifft2(np.fft.ifftshift(out))
     return out
 

holoaverage/rawalign.py
@@ -101,7 +101,7 @@ def rawAlign(series, qMax=None, verbose=0, roi=None, filter=None):
     return series
 
 
-def extractROI(series, roi, binning=1, verbose=0, dtype=None):
+def extractROI(series, roi, verbose=0, dtype=None):
     """
     Extracts ROI from a series the result as a new series.
 
@@ -112,17 +112,12 @@ def extractROI(series, roi, binning=1, verbose=0, dtype=None):
             The series to be reconstructed
         roi
             Region of interest in [px] (left, top, right, bottom)
-        binning
-            Binning (X,Y) to use
         dtype
             Type of ROI series, defaults to series type
     """
     if len(series.shape) != 2:
         raise ValueError("2D series expected.")
     shape = (roi[3] - roi[1], roi[2] - roi[0])
-    binning = np.diag(ScaleMatrix(2).set(binning).getMatrix()).astype(int)
-    if (shape[0] % binning[1]) != 0 or (shape[1] % binning[0]) != 0:
-        raise ValueError("ROI shape must be a multiple of binning.")
     if dtype is None:
         dtype = series.dtype
     raw_shift = series.attrs.get("raw_shift", np.zeros(series.indexShape + (2,), dtype=int))
@@ -131,14 +126,11 @@ def extractROI(series, roi, binning=1, verbose=0, dtype=None):
     dim_offset = series.attrs.get('dim_offset', np.zeros(2, dtype=float))
     if dim_scale.get() is not None:
         dim_offset = np.dot(dim_scale.getMatrix(), roi[0:2]) + dim_offset
-        dim_scale.set(dim_scale.getMatrix() * binning.astype(float))
-    old_binning = np.diag(ScaleMatrix(2).set(series.attrs.get('binning', 1)).getMatrix())
     overrides = {"dim_scale": dim_scale.getCompact(minRank=1),
-                 "dim_offset": dim_offset,
-                 "binning": old_binning * binning}
+                 "dim_offset": dim_offset}
 
     # Extracting
-    result = Series(series.indexShape, (shape[0] / binning[1], shape[1] / binning[0]), dtype=dtype)
+    result = Series(series.indexShape, shape, dtype=dtype)
     result.attrs.update(series.attrs)
     result.attrs.update(overrides)
     result.attrs['roi'] = roi
@@ -170,13 +162,11 @@ def extractROI(series, roi, binning=1, verbose=0, dtype=None):
         tmp[...] = np.mean(series[index].array)
         tmp[dy:dy + ny, dx:dx + nx] = series[index].array[sy:sy + ny, sx:sx + nx]
         data = DataSet(result.shape, dtype=dtype)
-        data.array[...] = 0
-        for i in range(binning[1]):
-            for j in range(binning[0]):
-                data.array[...] += tmp[i::binning[1], j::binning[0]]
+        data.array[...] = tmp
         data.attrs.update(series[index].attrs)
         data.attrs.update(overrides)
         data.attrs['roi'] = (_x + roi[0], _y + roi[1], _x + roi[2], _y + roi[3])
+        data.attrs['dim_offset'] = np.dot(dim_scale.getMatrix(), data.attrs['roi'][0:2]) + dim_offset
         result[index] = data
     if verbose > 0:
         print()

holoaverage/reconstruction.py
@@ -75,12 +75,13 @@ class HoloReconstructor(object):
         src_scale = grid.realSampling
         # Test for pixels out of desired region
         carrier_px = np.dot(src_scale, np.array((carrier[1], carrier[0]), dtype=float)) * np.array(self._grid.shape, dtype=float)
+        icarrier_px = np.floor(carrier_px).astype(int)
         ry, rx = self._grid.shape[0] // 2, self._grid.shape[1] // 2
         ny, nx = shape
         hy, hx = ny // 2, nx // 2
         dy, dx = 0, 0
-        sy = ry + int(carrier_px[0]) - hy
-        sx = rx + int(carrier_px[1]) - hx
+        sy = ry + icarrier_px[0] - hy
+        sx = rx + icarrier_px[1] - hx
         # print sx, sy, rx, ry, carrier_px, hx, hy
         if sy < 0:
             ny += sy
@@ -117,6 +118,7 @@ class HoloReconstructor(object):
         # Prepare FFT
         self._reco_grid = Grid(shape, realSampling=self._destScale, dtype=self._grid.complexType)
         self._reco_grid.prepareFFT(forwardFFT=False, backwardFFT=True, forwardRFFT=False, backwardRFFT=False)
+        self._reco_phase_wedge = np.dot(np.linalg.inv(src_scale), (carrier_px - icarrier_px) / np.array(self._grid.shape, dtype=float))
 
     def apply(self, data):
         if (data.shape != self._grid.shape):
@@ -128,12 +130,19 @@ class HoloReconstructor(object):
             tmp = np.fft.fftshift(fromHermite(self._grid.forwardRFFT(sub), sub.shape))
         else:
             tmp = sub
+
         # Extract and iFFT
         dx0, dy0, dx1, dy1 = self._destRect
         sx0, sy0, sx1, sy1 = self._srcRect
         side[dy0:dy1, dx0:dx1] = tmp[sy0:sy1, sx0:sx1] * self._mask
         result = DataSet(self._destShape, dtype=self._grid.complexType)
         result.array[...] = self._reco_grid.backwardFFT(np.fft.ifftshift(side))
+
+        # Correct for non-pixel aligned carrier
+        if not np.allclose(self._reco_phase_wedge, 0.0):
+            ry, rx = self._reco_grid.getRealGrid()
+            result.array[...] *= np.exp(-2.0j * np.pi * self._reco_phase_wedge[0] * ry) * np.exp(-2.0j * np.pi * self._reco_phase_wedge[1] * rx)
+
         result.attrs.update(data.attrs)
         result.attrs.update(self._reco_attrs)
         return result