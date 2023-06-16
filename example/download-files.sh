96f8e3d14fcc2db835af7f6fee28e34637ac8aa8
Tore Niermann <tore.niermann@tu-berlin.de>
2023-06-16 11:11:41 +0200

    Update Download URL

--- example/download-files.sh
+++ example/download-files.sh
@@ -2,7 +2,7 @@
 set -e
 TEMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'holoaverage-files-temp')
 echo "Temporarily downloading files into: $TEMP_DIR"
-curl -L --max-redirs 5 --output "$TEMP_DIR/GaN_holographic_focal_series.zip" https://depositonce.tu-berlin.de/bitstream/11303/7448/2/GaN_holographic_focal_series.zip
+curl -L --max-redirs 5 --output "$TEMP_DIR/GaN_holographic_focal_series.zip" https://depositonce.tu-berlin.de/bitstreams/e8a777a0-110b-4232-9ed1-0959418724a6/download
 unzip -d . "$TEMP_DIR/GaN_holographic_focal_series.zip" > /dev/null
 if [ ! -f ./GaN_holographic_focal_series/README.txt ]; then
     echo "Something went wrong, expecting GaN_holographic_focal_series/README.txt file."
