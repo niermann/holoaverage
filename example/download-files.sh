937a133eea8c90b5dfefde17d24732b67832f4af
Tore Niermann <tore.niermann@tu-berlin.de>
2023-06-16 11:07:08 +0200

    Download script for example files now follows redirects

--- example/download-files.sh
+++ example/download-files.sh
@@ -2,7 +2,7 @@
 set -e
 TEMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'holoaverage-files-temp')
 echo "Temporarily downloading files into: $TEMP_DIR"
-curl --output "$TEMP_DIR/GaN_holographic_focal_series.zip" https://depositonce.tu-berlin.de/bitstream/11303/7448/2/GaN_holographic_focal_series.zip
+curl -L --max-redirs 5 --output "$TEMP_DIR/GaN_holographic_focal_series.zip" https://depositonce.tu-berlin.de/bitstream/11303/7448/2/GaN_holographic_focal_series.zip
 unzip -d . "$TEMP_DIR/GaN_holographic_focal_series.zip" > /dev/null
 if [ ! -f ./GaN_holographic_focal_series/README.txt ]; then
     echo "Something went wrong, expecting GaN_holographic_focal_series/README.txt file."
